b0f6a6d15acb04d14ace682a0a64f194e055625d
Githusigan2002 <jeyarupangithusigan@gmail.com>
2026-02-08 08:55:02 +0530

    Fix: Correct comment syntax in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Login from "./pages/login";
+import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      //routes for navigate pages
+      {/* routes for navigate pages */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
